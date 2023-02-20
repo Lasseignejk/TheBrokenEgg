9842ca23d58f51a01e17ff07482dd9dbd879a701
Lasseignejk <jaye914@hotmail.com>
2023-02-20 17:32:31 -0500

    edits to about page and added carousel
    Added carousel component; worked on styling it a little, but it still just has pictures from a website, not the pics I want.

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Carousel, CarouselItem } from "./Carousel";
+
+const items = Array.from({ length: 10 }).map((_, i) => ({
+	id: i,
+	src: `https://picsum.photos/500?idx=${i}`,
+}));
 
 const HomePage = (): JSX.Element => {
-	return <div>Home</div>;
+	return (
+		<div>
+			<h2 className="text-xl pl-3">Popular Recipes</h2>
+			<div className="w-150px">
+				<Carousel
+					items={items}
+					renderItem={({ item, isSnapPoint }) => (
+						<CarouselItem key={item.id} isSnapPoint={isSnapPoint}>
+							<img src={item.src} className="" alt="" />
+						</CarouselItem>
+					)}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
